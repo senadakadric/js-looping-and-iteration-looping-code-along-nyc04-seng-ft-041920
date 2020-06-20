e60b8a2dbf64f94e3ff19ff266275621d9eb14d7
senadakadric <skadric@fordham.edu>
2020-06-20 16:24:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfNames, eventName) {
+  const messages = [];
+  for (let i = 0; i < arrayOfNames.length; i++) {
+    messages.push(
+      `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+  return messages;
+}
+
+function countDown(integer) {
+  for (let i = integer; i > -1; i--) {
+    console.log(i);
+  }
+}
